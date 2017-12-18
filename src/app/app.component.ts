6da26313c298dd43c481e11ca70c2aaf41e2621a
Alexey Sibrin <alexey.sibrin@gmail.com>
2017-12-19 00:05:29 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private _electronService: ElectronService) {
+  }
+
+  launchWindow() {
+    this._electronService.shell.openExternal('https://github.com/sibrin');
+  }
 }
